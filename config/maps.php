544ad93ed4870b8ae5ba39f411aad46ad7d22677
Ben Argo <ben@benargo.com>
2012-02-22 10:43:34 +0000

    Update of maps. 22 Feb 2012 10:42 GMT

--- config/maps.php
+++ config/maps.php
@@ -21,13 +21,98 @@
 if(defined('BASE_URI')) {
 
 	// Echo the page title
-	?><h1>Instagram</h1><?php
+	?><h1>Google Maps</h1><?php
 	
-	// Get the individual photo in JSON format
-	$xml = $city->map();
-	
-	print_r($xml);
+	foreach($cities as $city) { ?><section class="city map">
 
-}
+    <!-- you can use tables or divs for the overall layout -->
+    <table border=1>
+      <tr>
+        <td>
+           <div id="map" style="width: 550px; height: 450px"></div>
+        </td>
+        <td width = 150 valign="top" style="text-decoration: underline; color: #4444ff;">
+           <div id="side_bar"></div>
+        </td>
+      </tr>
+    </table>
 
-?>
+
+    <noscript><b>JavaScript must be enabled in order for you to use Google Maps.</b> 
+      However, it seems JavaScript is either disabled or not supported by your browser. 
+      To view Google Maps, enable JavaScript by changing your browser options, and then 
+      try again.
+    </noscript>
+
+
+    <script type="text/javascript">
+    //<![CDATA[
+
+    if (GBrowserIsCompatible()) {
+      // this variable will collect the html which will eventualkly be placed in the side_bar
+      var side_bar_html = "";
+    
+      // arrays to hold copies of the markers used by the side_bar
+      // because the function closure trick doesnt work there
+      var gmarkers = [];
+
+      // A function to create the marker and set up the event window
+      function createMarker(point,name,html) {
+        var marker = new GMarker(point);
+        GEvent.addListener(marker, "click", function() {
+          marker.openInfoWindowHtml(html);
+        });
+        // save the info we need to use later for the side_bar
+        gmarkers.push(marker);
+        // add a line to the side_bar html
+        side_bar_html += '<a href="javascript:myclick(' + (gmarkers.length-1) + ')">' + name + '<\/a><br>';
+        return marker;
+      }
+
+
+      // This function picks up the click and opens the corresponding info window
+      function myclick(i) {
+        GEvent.trigger(gmarkers[i], "click");
+      }
+
+
+      // create the map
+      var map = new GMap2(document.getElementById("map"));
+      map.addControl(new GLargeMapControl());
+      map.addControl(new GMapTypeControl());
+      map.setCenter(new GLatLng( 49.4008,1.4941), 5);
+
+
+      // Read the data from example.xml
+      GDownloadUrl("<?php echo BASE_URL; ?>/config/map.xml?city=<?php $city->id; ?>", function(doc) {
+        var xmlDoc = GXml.parse(doc);
+        var markers = xmlDoc.documentElement.getElementsByTagName("marker");
+          
+        for (var i = 0; i < markers.length; i++) {
+          // obtain the attribues of each marker
+          var lat = parseFloat(markers[i].getAttribute("lat"));
+          var lng = parseFloat(markers[i].getAttribute("lng"));
+          var point = new GLatLng(lat,lng);
+          var html = markers[i].getAttribute("html");
+          var label = markers[i].getAttribute("label");
+          // create the marker
+          var marker = createMarker(point,label,html);
+          map.addOverlay(marker);
+        }
+        // put the assembled side_bar_html contents into the side_bar div
+        document.getElementById("side_bar").innerHTML = side_bar_html;
+      });
+    }
+
+    else {
+      alert("Sorry, the Google Maps API is not compatible with this browser");
+    }
+
+    // This Javascript is based on code provided by the
+    // Community Church Javascript Team
+    // http://www.bisphamchurch.org.uk/   
+    // http://econym.org.uk/gmap/
+
+    //]]>
+    </script></section><?php }
+} ?>
